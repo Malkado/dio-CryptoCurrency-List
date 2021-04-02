ce17110aec2797e4851d59966f0fa8a6be5e76ed
Luiz Henrique <luizhsc.araujo@outlook.com>
2021-04-02 12:27:03 -0300

    feat: create dinamic list

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const  apiKey ={
+    key : '652cc601-9545-4367-80d9-ac685e01be3e'
+};
+
+
+function requestCryptoData(){
+    fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='+apiKey.key)
+    .then((response) =>{
+        if(!response.ok){
+            throw new Error('Erro ao executar a requisição, status '+ response.status);
+        }
+        return response.json();
+    })
+    .then(api =>{
+        var texto ="";
+
+        for (let i = 0; i < 10; i++) {
+
+            texto = texto + `
+            
+            <div class="media">
+
+            <img src="img/coin.jpg" class="align-self-center mr-3" alt="coin" width="100" heigth="60">
+            <div class="media-body">
+            <h5 class="mt-2">${api.data[i].name}</h5>
+            <p>${api.data[i].symbol}</p>
+            </div>
+            </div>
+            `;
+
+            document.getElementById("coins").innerHTML = texto;
+            
+
+        }
+    })
+    .catch(error=>{
+        console.error('Error: ', error);
+    })
+}
+requestCryptoData();
